fa0213a38f8ca200714da5d9518cf0945493c3ae
Rob Richardson <robrich@robrich.org>
2021-11-11 16:50:21 -0700

    Add right sized API example

---
+++ 10. right size/Program.cs
@@ -0,0 +1,44 @@
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+{
+    // TODO: swap with your data access / config read / api call
+    var text = File.ReadAllText("interestingConfig.json");
+    var json = JsonSerializer.Deserialize<InterestingConfig>(text);
+    ArgumentNullException.ThrowIfNull(json);
+    builder.Services.AddSingleton<InterestingConfig>(json);
+}
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapGet("/", (InterestingConfig config) => config);
+
+app.MapHealthChecks("/health");
+
+app.Map("/error", () => Results.StatusCode(500));
+app.Map("/{*url}", () => Results.NotFound());
+
+app.Run();
+
+public record class InterestingConfig(string thanks, string name, string email, string username, string largePic, string mediumPic, string thumbnailPic);
